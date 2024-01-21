a9d46c0d4f62d6d10c53d048c6b43eaf71f5ef26
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-01-21 13:56:44 +0800

    feat: change text when clicking button

--- app/src/main/java/com/example/gscindoorposition/MainActivity.kt
+++ app/src/main/java/com/example/gscindoorposition/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.gscindoorposition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var Gtext: TextView
+    private lateinit var Gbtn: Button
+    private var clickCnt = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Gtext = findViewById(R.id.GreetingView)
+    }
+
+    fun ChangeGreetingText(view: View) {
+        println("click"  + clickCnt.toString())
+        clickCnt++
+        Gtext.text = "click " + clickCnt.toString()
+        Toast.makeText(this, clickCnt.toString(), Toast.LENGTH_LONG).show()
+
     }
 }
