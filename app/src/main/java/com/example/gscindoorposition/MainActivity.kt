66f1ce075162ef9f435538b4ddb736006370a44e
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-01-21 14:18:24 +0800

    feat: change GUI to Indoor Position form

--- app/src/main/java/com/example/gscindoorposition/MainActivity.kt
+++ app/src/main/java/com/example/gscindoorposition/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Gtext = findViewById(R.id.GreetingView)
+        Gtext = findViewById(R.id.Title)
     }
 
     fun ChangeGreetingText(view: View) {
